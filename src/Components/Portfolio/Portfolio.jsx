3a51477ee31273aee73fef53dac881ff1a66ff1b
Mhezron <mhezron699@gmail.com>
2024-06-21 00:09:48 +0300

    added responsiveness

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,45 +1,92 @@
-import React from 'react'
-import './Portfolio.css'
-import bajaber from '../Assets/clients/bajaber.png'
-import bamburi from '../Assets/clients/bamburi cement.jpg'
-import cdorman from '../Assets/clients/c dorman.png'
-import gilanis from '../Assets/clients/c dorman.png'
-import nairobi from '../Assets/clients/Nairobi-City-County.png'
-import oillibya from '../Assets/clients/oil libya.jpeg'
-import pembe from '../Assets/clients/pembe.jpeg'
-import tononoka from '../Assets/clients/tononoka.jpg'
-import total from '../Assets/clients/total.jpeg'
-import tropikal from '../Assets/clients/tropikal.jpeg'
+// import React from 'react'
+// import './Portfolio.css'
+// import bajaber from '../Assets/clients/bajaber.png'
+// import bamburi from '../Assets/clients/bamburi cement.jpg'
+// import cdorman from '../Assets/clients/c dorman.png'
+// import gilanis from '../Assets/clients/c dorman.png'
+// import nairobi from '../Assets/clients/Nairobi-City-County.png'
+// import oillibya from '../Assets/clients/oil libya.jpeg'
+// import pembe from '../Assets/clients/pembe.jpeg'
+// import tononoka from '../Assets/clients/tononoka.jpg'
+// import total from '../Assets/clients/total.jpeg'
+// import tropikal from '../Assets/clients/tropikal.jpeg'
+
+// export const Portfolio = () => {
+//   return (
+//     <div className='portfolio'>
+//     {/* <h3>Portfolio</h3> */}
+//     <div className="logos-slide">
+//         <img src={bajaber} alt="" />
+//         <img src={bamburi} alt="" />
+//         <img src={cdorman} alt="" />
+//         <img src={gilanis} alt="" />
+//         <img src={nairobi} alt="" />
+//         <img src={oillibya} alt="" />
+//         <img src={pembe} alt="" />
+//         <img src={tononoka} alt="" />
+//         <img src={total} alt="" />
+//         <img src={tropikal} alt="" />
+//     </div>
+//     <div className="logos-slide">
+//         <img src={bajaber} alt="" />
+//         <img src={bamburi} alt="" />
+//         <img src={gilanis} alt="" />
+//         <img src={nairobi} alt="" />
+//         <img src={oillibya} alt="" />
+//         <img src={cdorman} alt="" />
+//         <img src={pembe} alt="" />
+//         <img src={tononoka} alt="" />
+//         <img src={total} alt="" />
+//         <img src={tropikal} alt="" />
+//     </div>
+    
+// </div>
+//   )
+// }
+
+
+
+import React from 'react';
+import './Portfolio.css';
+import bajaber from '../Assets/clients/bajaber.png';
+import bamburi from '../Assets/clients/bamburi cement.jpg';
+import cdorman from '../Assets/clients/c dorman.png';
+import gilanis from '../Assets/clients/c dorman.png';
+import nairobi from '../Assets/clients/Nairobi-City-County.png';
+import oillibya from '../Assets/clients/oil libya.jpeg';
+import pembe from '../Assets/clients/pembe.jpeg';
+import tononoka from '../Assets/clients/tononoka.jpg';
+import total from '../Assets/clients/total.jpeg';
+import tropikal from '../Assets/clients/tropikal.jpeg';
 
 export const Portfolio = () => {
   return (
     <div className='portfolio'>
-    {/* <h3>Portfolio</h3> */}
-    <div className="logos-slide">
-        <img src={bajaber} alt="" />
-        <img src={bamburi} alt="" />
-        <img src={cdorman} alt="" />
-        <img src={gilanis} alt="" />
-        <img src={nairobi} alt="" />
-        <img src={oillibya} alt="" />
-        <img src={pembe} alt="" />
-        <img src={tononoka} alt="" />
-        <img src={total} alt="" />
-        <img src={tropikal} alt="" />
+      <div className="logos-slide">
+        <img src={bajaber} alt="Bajaber" />
+        <img src={bamburi} alt="Bamburi" />
+        <img src={cdorman} alt="C Dorman" />
+        <img src={gilanis} alt="Gilanis" />
+        <img src={nairobi} alt="Nairobi" />
+        <img src={oillibya} alt="Oil Libya" />
+        <img src={pembe} alt="Pembe" />
+        <img src={tononoka} alt="Tononoka" />
+        <img src={total} alt="Total" />
+        <img src={tropikal} alt="Tropikal" />
+      </div>
+      <div className="logos-slide">
+        <img src={bajaber} alt="Bajaber" />
+        <img src={bamburi} alt="Bamburi" />
+        <img src={cdorman} alt="C Dorman" />
+        <img src={gilanis} alt="Gilanis" />
+        <img src={nairobi} alt="Nairobi" />
+        <img src={oillibya} alt="Oil Libya" />
+        <img src={pembe} alt="Pembe" />
+        <img src={tononoka} alt="Tononoka" />
+        <img src={total} alt="Total" />
+        <img src={tropikal} alt="Tropikal" />
+      </div>
     </div>
-    <div className="logos-slide">
-        <img src={bajaber} alt="" />
-        <img src={bamburi} alt="" />
-        <img src={gilanis} alt="" />
-        <img src={nairobi} alt="" />
-        <img src={oillibya} alt="" />
-        <img src={cdorman} alt="" />
-        <img src={pembe} alt="" />
-        <img src={tononoka} alt="" />
-        <img src={total} alt="" />
-        <img src={tropikal} alt="" />
-    </div>
-    
-</div>
-  )
-}
+  );
+};
+
